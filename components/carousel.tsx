35f9d19c9e1d0ac37119e356968c1d3e7d57cfdf
Stevenson Marquez Rangel <smarquez@kuenta.co>
2020-08-16 21:40:33 -0500

    show thumbnail pictures

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,34 +1,99 @@
 import { Picture } from "chillhood";
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import useSettings from "../context/hooks/use-settings";
 
-
 export interface CarouselProps {
-  pictures: Picture[]
+  pictures: Picture[];
 }
 
 const Carousel: FC<CarouselProps> = (props) => {
-  const settings = useSettings()
-  
-  const img = settings.cloudinary?.url(props.pictures[0].reference, {
-    width: 476,
-    height: 570,
-    crop: "pad"
-  })
+  const settings = useSettings();
+  const [current, setCurrent] = useState<Picture>(props.pictures[0]);
+  const [show, setShow] = useState(3);
+  const [start] = useState(0);
 
+  function thumbnail(reference: string) {
+    return settings.cloudinary?.url(reference, {
+      width: 90,
+      height: 90,
+      crop: "pad",
+    });
+  }
+
+  function preview(reference: string) {
+    return settings.cloudinary?.url(reference, {
+      width: 476,
+      height: 530,
+      crop: "pad",
+    });
+  }
+
+  useEffect(() => {
+    const resize = () => {
+      const width = document.documentElement.clientWidth;
+      if (width > 640) {
+        setShow(4)
+        return
+      } 
+      setShow(3)
+    }
+    window.addEventListener("resize", resize)
+    return () => window.removeEventListener("resize", resize)
+  }, [])
+
+  const pictures = props.pictures.slice(start, show);
 
   return (
     <div className="carousel">
-      <div className="current">
-        <img src={img} alt="" />
+      <div className="list">
+        {pictures.map(picture => (
+          <div
+            className="thumbnail"
+            key={picture.reference}
+            onMouseOver={() => setCurrent(picture)}
+            onClick={() => setCurrent(picture)}
+          >
+            <img src={thumbnail(picture.reference)} alt=""></img>
+          </div>
+        ))}
+      </div>
+      <div className="preview">
+        <img src={preview(current.reference)} alt="" />
       </div>
       <style jsx>{`
-        .current {
+        .carousel {
+          display: flex;
+          flex-direction: column-reverse;
+          align-items: center;
+          justify-content: center;
+        }
+        .list {
+          width: 100%;
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          overflow: hidden;
+        }
+        .thumbnail {
+          cursor: pointer;
+        }
+        .preview {
           text-align: center;
+          width: 80%;
+        }
+        .preview img {
           width: 100%;
         }
-        .current img {
-          width: 50%;
+        @media (min-width: 640px) {
+          .carousel {
+            flex-direction: row;
+            align-items: stretch;
+          }
+          .list {
+            justify-content: flex-start;
+            flex-direction: column;
+            width: 20%;
+          }
         }
       `}</style>
     </div>
